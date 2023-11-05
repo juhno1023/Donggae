ee4321827d8f24bb0b64d13786cc061340d4d6d0
eeheueklf <eeheueklf@gmail.com>
2023-11-05 13:20:14 +0900

    FeatL Sidebar Layout

--- donggae/src/components/mainpage/Mainpage.js
+++ donggae/src/components/mainpage/Mainpage.js
@@ -1,16 +1,15 @@
 import styles from "./Mainpage.module.css"
 import { useNavigate } from "react-router-dom";
 import Header from "./Header";
-
+import Sidebar from "./Sidebar";
 
 export default function Home() {
     const history = useNavigate();
     return (
         <div className={styles.default}>
-            <div className={styles.inner}>
-
             <Header />
-            
+            <div className={styles.inner}>
+                <Sidebar/>
             </div>
         </div>
 
