e73462c3c73e160211655e23d99d1e2bc7545f4a
dabeann <dabin4078@daum.net>
2023-11-07 10:34:23 +0900

    feat: User builer

---
+++ Donggae/src/main/java/Otwos/Donggae/domain/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package Otwos.Donggae.domain.member.repository;
+
+import Otwos.Donggae.DAO.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<User, Long> {
+
+}
