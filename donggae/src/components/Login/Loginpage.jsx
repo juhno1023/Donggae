0950a1b88ad0008aa53e87ede7cf0aa0da17aa55
HeoJunsang <junsang13454@gmail.com>
2023-11-07 23:02:50 +0900

    style : loginpage design modify

--- donggae/src/components/Login/Loginpage.jsx
+++ donggae/src/components/Login/Loginpage.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "./style.css";
+import donggae from '../../image/donggae.png';
+import github from '../../image/GitHub.png';
 
 export default function Home() {
     const history = useNavigate();
@@ -11,7 +13,7 @@ export default function Home() {
                 <div className="group" />
                 <div className="text-wrapper">로그인</div>
                 <div className="text-wrapper-2">회원가입</div>
-                <img className="image" alt="Image" src="src/image/donggae.png" />
+                <img className="image" alt="Image" src={github} />
               </div>
               <div className="overlap-group-wrapper">
                 <div className="overlap-group">
@@ -24,7 +26,7 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <img className="img" alt="Image" src="image-37.png" />
+              <img className="img" alt="Image" src={donggae} />
               <div className="text-wrapper-5">프로젝트 팀원을 찾으세요!</div>
             </div>
           </div>
