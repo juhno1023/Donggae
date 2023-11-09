cc2ddf2c383807581ae0921fc3828000d9c68442
dabeann <dabin4078@daum.net>
2023-11-09 10:22:12 +0900

    feat: apply #6

--- Donggae/src/main/java/Otwos/Donggae/domain/application/controller/ApplicationController.java
+++ Donggae/src/main/java/Otwos/Donggae/domain/application/controller/ApplicationController.java
@@ -1,7 +1,11 @@
 package Otwos.Donggae.domain.application.controller;
 
+import Otwos.Donggae.DTO.application.ApplyDTO;
 import Otwos.Donggae.domain.application.service.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,14 @@ public class ApplicationController {
 
     @Autowired
     private ApplicationService applicationService;
+
+    @PostMapping("/apply")
+    public ResponseEntity<?> applyForTeam(@RequestBody ApplyDTO applyDTO) {
+        try {
+            applicationService.applyFor(applyDTO);
+            return ResponseEntity.ok("지원 완료");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
--- Donggae/src/main/java/Otwos/Donggae/domain/application/service/ApplicationServiceImpl.java
+++ Donggae/src/main/java/Otwos/Donggae/domain/application/service/ApplicationServiceImpl.java
@@ -1,5 +1,6 @@
 package Otwos.Donggae.domain.application.service;
 
+import Otwos.Donggae.DAO.Recruit.RecruitPost;
 import Otwos.Donggae.DAO.User.User;
 import Otwos.Donggae.DTO.application.ApplyDTO;
 import Otwos.Donggae.domain.application.repository.ApplicationRepository;
@@ -10,17 +11,37 @@ import org.springframework.stereotype.Service;
 public class ApplicationServiceImpl implements ApplicationService{
     private ApplicationRepository applicationRepository;
     private MemberRepository memberRepository;
-    //private RecruitPostRepository recruitPostRepository;
+    private RecruitPostRepository recruitPostRepository;
 
-    public ApplicationServiceImpl(ApplicationRepository applicationRepository, MemberRepository memberRepository) {
+    public ApplicationServiceImpl(ApplicationRepository applicationRepository, MemberRepository memberRepository
+                                    , RecruitPostRepository recruitPostRepository) {
         this.applicationRepository = applicationRepository;
         this.memberRepository = memberRepository;
+        this.recruitPostRepository = recruitPostRepository;
     }
 
     @Override
     public void applyFor(ApplyDTO applyDTO) {
         User user = memberRepository.findUserByUserId(applyDTO.getUserId());
-        //RecruitPost recruitPost = recruitPostRepository.findRecruitPostByRecruitPostId(applyDTO.getRecruitPostId());
+        RecruitPost recruitPost = recruitPostRepository.findRecruitPostByRecruitPostId(applyDTO.getRecruitPostId());
         //예외처리 하고 저장
+        try {
+            validateUserAndRecruitPost(user, recruitPost);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        //해당 user와 recruitPost가 있으면 저장
+        //applicationId따로 안넣어도 알아서 들어가겠지?
+        applicationRepository.save(applyDTO.toEntity(user, recruitPost));
+    }
+
+    private void validateUserAndRecruitPost(User user, RecruitPost recruitPost) throws Exception {
+        if (user == null) {
+            throw new Exception("해당 user가 없습니다.");
+        }
+        if (recruitPost == null) {
+            throw new Exception("해당 recruitPost가 없습니다.");
+        }
     }
 }
