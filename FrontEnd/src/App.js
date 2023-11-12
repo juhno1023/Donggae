a562f3c5738c62bc6d43a627fb46b0a6f2c3d094
eeheueklf <eeheueklf@gmail.com>
2023-11-12 21:57:47 +0900

    Feat:시작페이지, 링크 설정

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './page/mainpage/Mainpage';
+import Main from './page/mainpage/Mainpage';
 import Posting from './page/Post/Posting';
 import Post from './page/Post/Post';
 import Application from './page/Application/Application';
@@ -14,11 +14,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/main" element={<Main />} />
           <Route path="/posting" element={<Posting />} />
           <Route path="/post" element={<Post />} />
           <Route path="/application" element={<Application />} />
-          <Route path="/Login" element={<Login />} />
           <Route path="/auth/github/callback" element={<AuthCallback />} />
           <Route path="/Signup" element={<Signup />}></Route>
           <Route path="/Leader" element={<Leader />}></Route>
