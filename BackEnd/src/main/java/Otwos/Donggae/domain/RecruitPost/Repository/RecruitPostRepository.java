c9c8ae1a85173ce9647a0bd7f6deaa9d11b09f31
heejjinkim <06.hjhj.12@gmail.com>
2023-11-15 21:57:55 +0900

    fix:recruit repository

--- BackEnd/src/main/java/Otwos/Donggae/domain/RecruitPost/Repository/RecruitPostRepository.java
+++ BackEnd/src/main/java/Otwos/Donggae/domain/RecruitPost/Repository/RecruitPostRepository.java
@@ -1,14 +1,20 @@
 package Otwos.Donggae.domain.RecruitPost.Repository;
 
 import Otwos.Donggae.DAO.Recruit.RecruitPost;
+import Otwos.Donggae.DAO.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RecruitPostRepository extends JpaRepository<RecruitPost, Integer> {
     RecruitPost findRecruitPostByRecruitPostId(int id);
 
     RecruitPost findRecruitPostByTitle(String title);
+
     RecruitPost save(RecruitPost recruitPost);
+
+    List<RecruitPost> findAllByIsCompleteAndUserId(Boolean isComplete, User user);
 
 }
--- BackEnd/src/main/java/Otwos/Donggae/domain/RecruitPost/Repository/info/RecruitPersonalityRepository.java
+++ BackEnd/src/main/java/Otwos/Donggae/domain/RecruitPost/Repository/info/RecruitPersonalityRepository.java
@@ -1,9 +1,8 @@
 package Otwos.Donggae.domain.RecruitPost.Repository.info;
 
-import Otwos.Donggae.DAO.Recruit.RecruitLanguage;
+import Otwos.Donggae.DAO.Recruit.Identifier.RecruitPersonalityPK;
 import Otwos.Donggae.DAO.Recruit.RecruitPersonality;
-import Otwos.Donggae.DAO.Recruit.RecruitPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecruitPersonalityRepository extends JpaRepository<RecruitPersonality, Long> {
+public interface RecruitPersonalityRepository extends JpaRepository<RecruitPersonality, RecruitPersonalityPK> {
 }
