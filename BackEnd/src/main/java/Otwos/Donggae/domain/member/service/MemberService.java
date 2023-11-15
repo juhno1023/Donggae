738b8ed67d3e69f05ba0fa2d315754e7f82a8ea8
dabeann <dabin4078@daum.net>
2023-11-15 12:46:39 +0900

    feat: validate Duplicate GithubName #40

--- BackEnd/src/main/java/Otwos/Donggae/domain/member/service/MemberService.java
+++ BackEnd/src/main/java/Otwos/Donggae/domain/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package Otwos.Donggae.domain.member.service;
 
 import Otwos.Donggae.DTO.member.register.SignUpDTO;
+import Otwos.Donggae.DTO.member.register.ValidGithubIdRequest;
 
 public interface MemberService {
     public void register(SignUpDTO signUpDTO);
@@ -8,4 +9,5 @@ public interface MemberService {
     public Integer checkUserSignUp(String githubId);
 
     public String getUserEmail(int userId);
+    public void validGithubName(ValidGithubIdRequest request);
 }
