022a840f9445f0ce63052e2e4b3fcc8064b71a7f
juhno1023 <hyeondnr1023@gmail.com>
2023-11-17 22:33:23 +0900

    Revert "feat: 내가 속한 팀 페이지 조회 #19"
    This reverts commit 0703019f5cc68fb26f37687645389bab731cc1dc.

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,35 +1,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
-import Signup from "./page/Signup/Signup";
-import Login from './page/Login/Login';
-import AuthCallback from './page/Login/AuthCallback';
-
 import Main from './page/mainpage/Mainpage';
 import Posting from './page/Post/Posting';
 import Post from './page/Post/Post';
 import Application from './page/Application/Application';
-
+import Login from './page/Login/Login';
+import Signup from "./page/Signup/Signup";
 import Leader from "./page/Project/Leader";
 import TeamInfo from "./page/Project/TeamInfo";
-import UserTeam from "./page/Project/UserTeam";
-
+import AuthCallback from './page/Login/AuthCallback';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Login />} />
-          <Route path="/auth/github/callback" element={<AuthCallback />} />
           <Route path="/main" element={<Main />} />
           <Route path="/posting" element={<Posting />} />
           <Route path="/post" element={<Post />} />
           <Route path="/application" element={<Application />} />
-          <Route path="/leader" element={<Leader />} />
-          <Route path="/teaminfo" element={<TeamInfo />} />
-          <Route path="/userteam" element={<UserTeam />} />
+          <Route path="/auth/github/callback" element={<AuthCallback />} />
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/leader" element={<Leader />}></Route>
+          <Route path="/teaminfo" element={<TeamInfo />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
