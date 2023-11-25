641ded12538d450210b2babb7a44a0ca2df0ac71
eeheueklf <eeheueklf@gmail.com>
2023-11-25 23:35:35 +0900

    feat: 검색창 #69

--- FrontEnd/src/components/_Layout/Header.jsx
+++ FrontEnd/src/components/_Layout/Header.jsx
@@ -1,14 +1,11 @@
 import styles from "./Header.module.css"
-import { useNavigate } from "react-router-dom";
+import { useNavigate , Link} from "react-router-dom";
 import bgImg from '../../image/donggae.png';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Header() {
-    const history = useNavigate();
-    const menus = [
-        {name:"hot추천", path:"/main"},
-        {name:"문화행사", path:"/posting"},
-    ]
+
+
     return (
         <div className={styles.header}>
             <div className={styles.contents}>
@@ -18,10 +15,14 @@ export default function Header() {
                         <div className={styles.title}>Donggae</div>
                         <div className={styles.subtitle}><b>동</b>국대 <b>개</b>발자들</div>
                     </div>
+                    <div className={styles.search}>
+                        <input type="text" placeholder="검색어 입력"/>
+                        <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"/>
+                    </div>
                 </div>
                 <nav className={styles.navigation}>
                     <ul>
-                    <li><Link to={menus[0].path}>메인</Link></li>
+                    <li><Link to='/main'>메인</Link></li>
                     <li><Link to='/posting'>posting</Link></li>
                     <li><Link to='/leader'>leader</Link></li>
                     <li><Link to='/userteam'>myteam</Link></li>
