d6e64a5426ec5edf0a566e78a860a996f2a3be17
HeoJunsang <junsang13454@gmail.com>
2023-12-01 22:54:47 +0900

    feat : 메인페이지 디자인, 데이터 조회와 수정 fetch

--- FrontEnd/src/components/_Layout/Sidebars.jsx
+++ FrontEnd/src/components/_Layout/Sidebars.jsx
@@ -27,6 +27,7 @@ const Sidebar = () => {
                 <li className={`sidebar__menu__item`}><Link to='/rank'>rank</Link></li>
                 <li className={`sidebar__menu__item`}><Link to='/teaminfo'>내가 속한 팀</Link></li>
                 <li className={`sidebar__menu__item`}><Link to='/mypage'>마이페이지</Link></li>
+                <li className={`sidebar__menu__item`}><Link to='/recruit'>팀원 모집 검색</Link></li>
                 <li className={`sidebar__menu__item`} onClick={logout}><Link to='/'>로그아웃</Link></li>
                 </ul>
             </nav>
