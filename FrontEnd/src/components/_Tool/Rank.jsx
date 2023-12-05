30caa4d9fb4d9d18a2d6f8a38646e3a8dc7d24dd
HeoJunsang <junsang13454@gmail.com>
2023-12-05 22:37:21 +0900

    feat : 마이페이지 끝, 랭킹 페이지 끝, 테스트 문제 받아오기 fetch

--- FrontEnd/src/components/_Tool/Rank.jsx
+++ FrontEnd/src/components/_Tool/Rank.jsx
@@ -1,17 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import styles from "./Rank.module.css"
 
+import donggae from '../../image/donggae.png';
+
+import DongD from '../../image/DongDonggae.png';
+import BronzeD from '../../image/BronzeDonggae.png';
+import SilverD from '../../image/SilverDonggae.png';
+import GoldD from '../../image/GoldDonggae.png';
+import DiamondD from '../../image/DiamondDonggae.png';
+
+const selectImage = (condition) => {
+    // 조건에 따라 다른 이미지를 선택하는 함수
+    if (condition === '다이아동개') {
+      return DiamondD;
+    } 
+    else if (condition === '황금동개') {
+        return GoldD;
+    } 
+    else if (condition === '은동개') {
+        return SilverD;
+    }
+    else if (condition === '동동개') {
+        return BronzeD;
+    }
+    else {
+        return DongD;
+    };
+
+}
+
+
 const Rank = ({ rankNo, rank, id, field, score, tier}) => {
 
     return (
         <> 
             <tr>
                 <td className={styles.number}>{rankNo}</td>
+                <td className={styles.number}><img
+                                        src={selectImage(rank)}
+                                        alt="Image"
+                                        /></td>
                 <td className={styles.name}>{id}</td>
                 <td className={styles.points}>{score}<img class="gold-medal" src="https://github.com/malunaridev/Challenges-iCodeThis/blob/master/4-leaderboard/assets/gold-medal.png?raw=true" alt="gold medal"/></td>
                 <td className={styles.name}>{field}</td>
                 <td className={styles.name}>{tier}</td>
-                    
              </tr>
         </>
     );
--- FrontEnd/src/page/Rankpage/Rankpage.jsx
+++ FrontEnd/src/page/Rankpage/Rankpage.jsx
@@ -6,6 +6,9 @@ import Sidebar from "../../components/_Layout/Sidebars";
 import Rank from '../../components/_Tool/Rank';
 import tearImg from '../../image/tearInfo.png';
 
+
+
+
 export default function Mypage() {
     const history = useNavigate();
 
@@ -39,6 +42,9 @@ export default function Mypage() {
             handleClick();
         },[]);
 
+
+        
+
     return (
         <div className={styles.default}>
             <Header />
@@ -52,6 +58,7 @@ export default function Mypage() {
                     <table>
                         <tr>
                             <td>종합순위</td>
+                            <td>동개등급</td>
                             <td>id</td>
                             <td>점수</td>
                             <td>tier</td>
